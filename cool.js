b62e99092b84981beb83330435614abc010519cb
Diana Carolina Suarez <dianacarolinasuarez@Dianas-iMac.local>
2021-12-15 17:05:45 -0500

    add package mgmt files

---
+++ cool.js
@@ -0,0 +1,11 @@
+/***
+ * Excerpted from "Design and Build Great Web APIs",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/maapis for more book information.
+***/
+var cool = require('cool-ascii-faces');
+
+console.log(cool());
